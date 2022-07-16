addbc27ad6aeee0cc8f40685cf1232e38acd3afe
Abel Mitiku <abelmgetnet@gmail.com>
2022-07-17 00:39:17 +0300

    added the main home page

--- flutterdapp/lib/main.dart
+++ flutterdapp/lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// 
+//
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -46,7 +46,7 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-// 
+//
 
 class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController _passwordController = TextEditingController();
@@ -96,90 +96,89 @@ class _MyHomePageState extends State<MyHomePage> {
                 },
               ),
             ),
-Padding(
-padding: const EdgeInsets.only(
-left: 50,
-right: 50,
-bottom: 10,
-),
-child: TextFormField(
-controller: _passwordController,
-obscureText: true,
-style: const TextStyle(
-color: Colors.black,
-fontSize: 22,
-),
-cursorColor: Colors.black,
-decoration: const InputDecoration(
-hintText: 'Input Password',
-border: UnderlineInputBorder(
-borderSide: BorderSide(color: Colors.black),
-),
-focusedBorder: UnderlineInputBorder(
-borderSide: BorderSide(color: Colors.black),
-),
-),
-),
-),
-Text("(Password must be 16 Characters)"),
-SizedBox(
-height: 20.0,
-),
-Container(
-width: MediaQuery.of(context).size.width / 1.3,
-height: 50,
-decoration: BoxDecoration(
-borderRadius: BorderRadius.circular(35.0),
-color: Colors.amber,
-),
-child: MaterialButton(
-onPressed: () {
-if (_passwordController.text.length == 16) {
-if (_value == 1) {
-Navigator.push(
-context,
-MaterialPageRoute(
-builder: (context) => const ParentPage(),
-),
-);
-} else if (_value == 2) {
-Navigator.push(
-context,
-MaterialPageRoute(
-builder: (context) => const ChildPage(),
-),
-);
-} else {
-Text("Invalid User");
-}
-//print('Password: ' + _passwordController.text);
-print('Secret Key: s***************');
-password = _passwordController.text;
-} else {
-setState(() {
-error =
-"Invalid length: \nPlease input 16 character secret
-password";
-});
-}
-},
-child: const Text('Login'),
-),
-),
-SizedBox(
-height: 20.0,
-),
-Text(
-error,
-style: TextStyle(color: Colors.red[900]),
-textAlign: TextAlign.center,
-),
-SizedBox(
-height: 20.0,
-),
-],
-),
-),
-);
-}
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 50,
+                right: 50,
+                bottom: 10,
+              ),
+              child: TextFormField(
+                controller: _passwordController,
+                obscureText: true,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 22,
+                ),
+                cursorColor: Colors.black,
+                decoration: const InputDecoration(
+                  hintText: 'Input Password',
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                ),
+              ),
+            ),
+            Text("(Password must be 16 Characters)"),
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.3,
+              height: 50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(35.0),
+                color: Colors.amber,
+              ),
+              child: MaterialButton(
+                onPressed: () {
+                  if (_passwordController.text.length == 16) {
+                    if (_value == 1) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const ParentPage(),
+                        ),
+                      );
+                    } else if (_value == 2) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const ChildPage(),
+                        ),
+                      );
+                    } else {
+                      Text("Invalid User");
+                    }
+                    //print('Password: ' + _passwordController.text);
+                    print('Secret Key: s***************');
+                    password = _passwordController.text;
+                  } else {
+                    setState(() {
+                      error =
+                          "Invalid length: \nPlease input 16 character secret password";
+                    });
+                  }
+                },
+                child: const Text('Login'),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              error,
+              style: TextStyle(color: Colors.red[900]),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
